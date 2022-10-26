1f5f10431a14679884e5d2afc8acdf32c5c1ccc5
Pavel Bolotov <axsxbarker@gmail.com>
2022-10-26 18:35:03 +0200

    сверстан компонент "Логин"

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,52 @@
 import style from './Login.module.css'
 import UserAvatar from '../../images/user-menu.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const Login = () => {
-
-  const [isActive, setActive] = useState(false);
-
-  const toggleClass = () => {
-    setActive(!isActive);
-
-  };
-  return (<div className={style.login_wrapper}>
-    <img src={UserAvatar} alt="user avatar" className={style.user_avatar} />
-    <div className={isActive ? style.icon + ' ' + style.isopen : style.icon}
-      onClick={toggleClass} ></div>
-  </div>);
+function useDelayUnmount(isMounted, delayTime) {
+  const [showDiv, setShowDiv] = useState(false);
+  useEffect(() => {
+    let timeoutId;
+    if (isMounted && !showDiv) {
+      setShowDiv(true);
+    } else if (!isMounted && showDiv) {
+      timeoutId = setTimeout(() => setShowDiv(false), delayTime);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [isMounted, delayTime, showDiv]);
+  return showDiv;
 }
 
-export default Login;
+const mountedStyle = { animation: "inAnimation 250ms ease-in" };
+const unmountedStyle = {
+  animation: "outAnimation 270ms ease-out",
+  animationFillMode: "forwards"
+};
+
+export default function Login() {
+  const [isMounted, setIsMounted] = useState(false);
+  const showDiv = useDelayUnmount(isMounted, 250);
+
+  return (
+    <div className={style.login_wrapper}>
+      <img src={UserAvatar} alt="user avatar" className={style.user_avatar} />
+      <div onClick={() => setIsMounted(!isMounted)} className={isMounted ? style.icon + ' ' + style.isopen : style.icon} />
+      {showDiv &&
+        <div
+          className={style.login_dropdown}
+          style={isMounted ? mountedStyle : unmountedStyle}
+        >
+          <button className={style.login_dropdown_button}>Profile</button>
+          <button className={style.login_dropdown_button}>Log Out</button>
+        </div>
+
+      }
+    </div>
+  );
+}
+
+
+
+
+
+
+
